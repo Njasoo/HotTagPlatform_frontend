f434295def8b1440189ce3e7a99f6ec8ca8ce437
Njaso <jason749573@163.com>
2025-11-18 19:47:18 +0800

    chang delay to 500

--- src/composables/useThrottle.ts
+++ src/composables/useThrottle.ts
@@ -1,5 +1,5 @@
 // 快速点击下一页的时候，会导致页码和实际内容不同步，这时候需要利用节流来限制短时间内频繁操作的行为
-export const useThrottle = (fn: Function, delay: number = 300) => {
+export const useThrottle = (fn: Function, delay: number = 500) => {
   let last = 0;
   return (...args: any[]) => {
     const now = Date.now();
