23f0f1ea0bcda8a829e3c0fb270e48b58257e602
Njaso <jason749573@163.com>
2025-11-19 18:23:44 +0800

    add loading

--- src/components/WordCloud.vue
+++ src/components/WordCloud.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="mt-3 bg-white w-[95%] container mx-auto shadow-sm p-4">
+  <span
+    v-if="loading"
+    class="mx-auto block loading loading-spinner loading-xl mt-3"
+  ></span>
+  <div v-else class="mt-3 bg-white w-[95%] container mx-auto shadow-sm p-4">
     <img class="mx-auto w-150 h-150" :src="imageUrl" alt="" />
   </div>
 </template>
@@ -11,12 +15,14 @@ const props = defineProps<{
   selectedValue: string;
 }>();
 const imageUrl = ref<string>("");
+const loading = ref<boolean>(true);
 
 onMounted(() => {
   get_wordcloud_url(props.selectedValue)
     .then((res: any) => {
       console.log("wordcloud res:", res.data);
       imageUrl.value = res.data.url;
+      loading.value = false;
     })
     .catch((err: any) => console.error(err));
 });
@@ -24,10 +30,12 @@ onMounted(() => {
 watch(
   () => props.selectedValue,
   (newVal: string) => {
+    loading.value = true;
     get_wordcloud_url(newVal)
       .then((res: any) => {
         console.log("wordcloud res:", res.data);
         imageUrl.value = res.data.url;
+        loading.value = false;
       })
       .catch((err: any) => console.error(err));
   }
