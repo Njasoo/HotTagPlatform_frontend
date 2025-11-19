c754926d40b30496e64e3940e0174801c48699d9
Njaso <jason749573@163.com>
2025-11-19 19:14:34 +0800

    change history to hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createWebHashHistory, createRouter } from "vue-router";
 import Home from "../pages/Home.vue";
 
 const routes = [
@@ -23,7 +23,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
