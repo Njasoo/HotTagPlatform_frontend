3b46e5767ebfb0b598e3314504a7d2f621caf3d8
Njaso <jason749573@163.com>
2025-11-26 23:51:19 +0800

    fix bug

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -7,7 +7,7 @@
     <div class="mx-auto w-[95%] mt-3">
       <ul class="list bg-base-100 shadow-sm">
         <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">
-          {{ selectedPlatform }}今日热榜
+          {{ newsStore.current_platform_zh }}今日热榜
         </li>
 
         <!-- list-row包含flex容器 -->
@@ -28,18 +28,18 @@
     </div>
     <div class="join flex justify-center mt-3">
       <button
-        class="text-base-content join-item btn bg-base-100 hover:bg-accent"
+        class="text-base-content join-item btn bg-base-100 hover:bg-accent shadow-sm"
         id="prev-btn"
         :disabled="prevPage == null"
         @click="prevPageHandle"
       >
         «
       </button>
-      <button class="join-item btn bg-base-100">
+      <button class="join-item btn bg-base-100 shadow-sm">
         {{ currentPageNumber }}
       </button>
       <button
-        class="text-base-content join-item btn bg-base-100 hover:bg-accent"
+        class="text-base-content join-item btn bg-base-100 hover:bg-accent shadow-sm"
         id="next-btn"
         :disabled="nextPage == null"
         @click="nextPageHandle"
@@ -56,7 +56,9 @@ import { getSourceList } from "@/api/source";
 import request from "@/api/request";
 import router from "@/router";
 import { useThrottleFn } from "@vueuse/core";
+import { useNewsStore } from "@/stores/news";
 
+const newsStore = useNewsStore();
 const page2category: { path: string; category: string }[] = [
   { path: "/culture", category: "文化" },
   { path: "/internation", category: "国际新闻" },
@@ -72,7 +74,7 @@ const loading = ref(false);
 const currentPageNumber = ref(1);
 const prevPage = ref("");
 const nextPage = ref("");
-const selectedPlatform = ref("哔哩哔哩");
+// const selectedPlatform = ref("哔哩哔哩");
 interface NewsItem {
   // 规定出来看的，实际上运行不会做类型检查，直接被覆盖了，不过别人一看代码就知道我这个数组里面要存什么东西
   id: number;
@@ -145,7 +147,7 @@ watch(
     // 这里是直接暴力的，因为数据量比较小，所以用map可能常数还比较大的
     for (const item of sourceList.value) {
       if (item.value == newVal) {
-        selectedPlatform.value = item.name;
+        newsStore.current_platform_zh = item.name;
       }
     }
   }
--- src/stores/news.ts
+++ src/stores/news.ts
@@ -3,6 +3,7 @@ import { defineStore } from "pinia";
 export const useNewsStore = defineStore("news", {
   state: () => ({
     current_platform: "bilibili",
+    current_platform_zh: "哔哩哔哩",
   }),
   persist: true,
 });
